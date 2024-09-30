6ce7b9ede41f3a1f2472a8ffb2df6c3cbfd27d55
Milton <milton_4541@live.com>
2024-09-30 13:34:26 -0300

    Final

---
+++ src/components/CalorieTracker.tsx
@@ -0,0 +1,42 @@
+import { useMemo } from "react"
+import { Activity } from "./types"
+import CalorieDisplay from "./CalorieDisplay"
+
+type CalorieTrackerPropr = {
+    activities: Activity[]
+
+}
+export default function CalorieTracker({activities}: CalorieTrackerPropr) {
+ 
+    //contadores
+    const caloriesConsumed = useMemo (()=>activities.reduce((total,activity)=> activity.category === 1 ? total + activity.calories : total, 0), [activities])
+   
+    const caloriesBurned = useMemo (()=>activities.reduce((total,activity)=> activity.category === 2 ? total + activity.calories : total, 0), [activities])
+   
+    const netCalories = useMemo (() => caloriesConsumed - caloriesBurned, [activities])
+    return (
+    <>
+        <h2 className="text-4xl font-black text-white text-center">Resumen de calorias</h2>
+
+        <div className="flex flex-col items-center md:flex-row mt-10 md:justify-between gap-5">
+            <CalorieDisplay
+                calories={caloriesConsumed}
+                text="Consumidas"
+            />
+
+            <CalorieDisplay
+                calories={caloriesBurned}
+                text="Ejercicio"
+            />
+
+            <CalorieDisplay
+                calories={netCalories}
+                text="Diferencia"
+            />
+        </div>
+
+        
+    </>
+
+)
+}
